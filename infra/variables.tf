ead49d17aad76ab67490162117f478de1103d649
Eirik <eirik.am@hotmail.com>
2022-12-08 14:14:22 +0100

    drar hjem og tar lunchpause

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,7 +1,9 @@
 variable "candidate_id" {
+  default = "1045"
   type = string
 }
 
 variable "candidate_email"  {
+  default = "eiam001@student.kristiania.no"
   type = string
 }
--- src/main/java/no/shoppifly/ShoppingCartController.java
+++ src/main/java/no/shoppifly/ShoppingCartController.java
@@ -1,15 +1,27 @@
 package no.shoppifly;
 
+import io.micrometer.core.instrument.DistributionSummary;
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.distribution.DistributionStatisticConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
 import java.util.List;
+import java.util.function.Supplier;
 
 @RestController()
 public class ShoppingCartController {
 
+    private MeterRegistry meterRegistry;
     @Autowired
     private final CartService cartService;
+
+    //private MeterRegistry meterRegistry;
+
 
     public ShoppingCartController(CartService cartService) {
         this.cartService = cartService;
@@ -27,6 +39,16 @@ public class ShoppingCartController {
      */
     @PostMapping(path = "/cart/checkout")
     public String checkout(@RequestBody Cart cart) {
+
+        meterRegistry.counter("checkout").increment();
+        meterRegistry.counter("checkout", "cart", cart.getId()).increment();
+        DistributionStatisticConfig.builder()
+                .percentilesHistogram(true)
+                .build();
+        DistributionSummary.builder("latency")
+                .publishPercentileHistogram()
+                .register(meterRegistry);
+        this.meterRegistry.timer("checkout_latency").record(() -> cartService.checkout(cart));
         return cartService.checkout(cart);
     }
 
@@ -36,9 +58,16 @@ public class ShoppingCartController {
      *
      * @return the updated cart
      */
-    @PostMapping(path = "/cart")
-    public Cart updateCart(@RequestBody Cart cart) {
-        return cartService.update(cart);
+    @PostMapping(path = "/cart", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Cart> updateCart(@RequestBody Cart cart) {
+
+        meterRegistry.counter("update_cart").increment();
+        cart.setItems(cart.getItems());
+        cart.setId(cart.getId());
+
+        return new ResponseEntity<>(cartService.update(cart), HttpStatus.OK);
+        //return ResponseEntity.ok(cartService.update(cart));
+        //return cartService.update(cart);
     }
 
     /**
@@ -46,10 +75,14 @@ public class ShoppingCartController {
      *
      * @return
      */
-    @GetMapping(path = "/carts")
+    @GetMapping(path = "/carts", consumes = "application/json", produces = "application/json")
     public List<String> getAllCarts() {
+     //   Gauge.builder("carts", (Supplier<Number>) getAllCarts());
+        meterRegistry.counter("get_all_carts").increment(); // Increment a metric called "get_all_carts" every time this is called
         return cartService.getAllsCarts();
     }
+
+
 
 
 }
