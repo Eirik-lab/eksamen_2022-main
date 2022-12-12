852a170d1f65356fce27bba1e82e9be958c72a44
Eirik-lab <eirik.am@hotmail.com>
2022-12-12 07:35:59 +0100

    alarm

--- infra/sns_topic_for_alarm.tf
+++ infra/sns_topic_for_alarm.tf
@@ -7,3 +7,21 @@ resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
   protocol  = "email"
   endpoint  = var.candidate_email
 }
+
+resource "aws_cloudwatch_metric_alarm" "x300sectreshold" {
+  alarm_name                = "cart-afk-time-must-be-5-min"
+  namespace                 = var.candidate_id
+  metric_name               = "carts.value"
+
+  comparison_operator       = "GreaterThanThreshold"
+  threshold                 = "5"
+  evaluation_periods        = "2"
+  period                    = "300"
+
+  statistic                 = "Maximum"
+
+  alarm_description         = "This alarm goes off as soon as the cart is idle for more than 5 minutes"
+  insufficient_data_actions = []
+  alarm_actions       = [aws_sns_topic.alarms.arn]
+}
+
